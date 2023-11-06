7e926b75e5d32c95faa21131f62f0c17871960bb
rico <challykiddoh@gmail.com>
2023-11-06 18:59:45 +0300

    fix service

--- src/main/java/com/ShopsService/shopsService/Controller/AdminController.java
+++ src/main/java/com/ShopsService/shopsService/Controller/AdminController.java
@@ -1,16 +1,14 @@
 package com.ShopsService.shopsService.Controller;
-
+import com.ShopsService.shopsService.Exceptions.NotFoundException;
 import com.ShopsService.shopsService.Services.ShopServiceImpl;
 import com.ShopsService.shopsService.Tdo.ShopRequest;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin")
@@ -25,5 +23,18 @@ public class AdminController {
         return ResponseEntity.status(HttpStatus.CREATED.value())
                 .body(newShop);
 
+    }
+    @PutMapping("/update/{storeNumber}")
+    public ResponseEntity<?> updateShopInfo(@RequestBody ShopRequest shopRequest,@PathVariable("storeNumber") String storeNumber) throws EntityNotFoundException {
+
+        var Res=   shopService.updateShop(shopRequest,storeNumber);
+        return  ResponseEntity.status(HttpStatus.CREATED.value()).body(Res);
+    }
+    @DeleteMapping("/delete/{storeNumber}")
+    public  ResponseEntity<?> deleteShop(@PathVariable("storeNumber") String storeNumber) throws NotFoundException {
+
+        var res=  shopService.deleteShop(storeNumber);
+        System.out.println("the end point was hit");
+        return  ResponseEntity.status(HttpStatus.CREATED).body(res);
     }
 }
--- src/main/java/com/ShopsService/shopsService/Controller/ShopsController.java
+++ src/main/java/com/ShopsService/shopsService/Controller/ShopsController.java
@@ -2,17 +2,16 @@ package com.ShopsService.shopsService.Controller;
 
 import com.ShopsService.shopsService.Exceptions.NotFoundException;
 import com.ShopsService.shopsService.Services.ShopServiceImpl;
-import com.ShopsService.shopsService.Tdo.ShopRequest;
+
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -26,8 +25,6 @@ public class ShopsController {
         var res=shopService.findAll();
         return  ResponseEntity.status(HttpStatus.FOUND).body(res);
     }
-
-
     @GetMapping("/{storeNumber}")
 
     public ResponseEntity<?> findShopById(@PathVariable("storeNumber") String storeNumber) throws  EntityNotFoundException{
@@ -41,33 +38,14 @@ public class ShopsController {
 
 
         }
-    @PostMapping("/new")
 
-    public ResponseEntity<?> createNewShop(@RequestBody @Valid ShopRequest request) throws HttpMessageNotReadableException {
-
-        var newShop=shopService.createNewShop(request);
-        return ResponseEntity.status(HttpStatus.CREATED.value())
-                .body(newShop);
-
-    }
 
 //        update the shop details
 
-    @PutMapping("/update/{storeNumber}")
-    public ResponseEntity<?> updateShopInfo(@RequestBody ShopRequest shopRequest,@PathVariable("storeNumber") String storeNumber) throws EntityNotFoundException{
-
-     var Res=   shopService.updateShop(shopRequest,storeNumber);
-        return  ResponseEntity.status(HttpStatus.CREATED.value()).body(Res);
-    }
 
 
-    @DeleteMapping("/delete/{storeNumber}")
-    public  ResponseEntity<?> deleteShop(@PathVariable("storeNumber") String storeNumber) throws NotFoundException{
 
-   var res=  shopService.deleteShop(storeNumber);
-System.out.println("the end point was hit");
-   return  ResponseEntity.status(HttpStatus.CREATED).body(res);
-    }
+
 }
 
 
